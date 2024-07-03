abe70412b72b03a182ecc3fe2847a6f8726ee3b2
JhinTsako <hatedblaster@gmail.com>
2024-07-03 19:33:01 +0300

    First Update
    Some minor graphical changes and the implementation of 1/3 basic attack animations.

--- Assets/scripts/AnimationString.cs
+++ Assets/scripts/AnimationString.cs
@@ -10,4 +10,9 @@ internal class AnimationString
     internal static string isMoving = "isMoving";
     internal static string isGrounded = "isGrounded";
     internal static string Jump = "Jump";
+    internal static string isWalking = "isWalking";
+    internal static string attackTrigger = "attack";
+    internal static string canMove = "canMove";
+    internal static string isOnWall = "isOnWall";
+    internal static string isOnCeiling = "isOnCeiling";
 }
